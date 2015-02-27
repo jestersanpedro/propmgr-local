c00de49af882f59cf5bdb05f177ccceb8954ef3e
jestersanpedro <jester.sanpedro@tooltwist.com>
2015-02-27 11:46:33 +0800

    initial commit

---
+++ documentation/build-myrp.sh
@@ -0,0 +1,26 @@
+!/bin/sh
+#
+#	Install files to the Gihub Pages directory (<projectname>.pages) and push to the remote repository
+#
+bin=`dirname $0`
+
+PROJECT_NAME=propmgr-local
+
+# Generate stuff
+(
+	cd $bin/restapis
+	jekyll build --destination ../../../propmgr-local/restapis/
+)
+
+# push the github pages
+(
+	cd ~/Development/propmgr-carte-local/propmgr-local
+	git add .
+	git commit -m "Commit by build.sh"
+	git push origin master
+	git checkout gh-pages
+	git merge master
+	git push origin gh-pages
+	git checkout master
+)
+exit 0
